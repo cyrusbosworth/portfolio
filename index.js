8f95a7f9906b54a87de16ecc85be46921623f9e2
Cyrus Kurtz <cyrus.bosworth@gmail.com>
2020-02-21 02:10:59 -0500

    responsive front page

--- index.js
+++ index.js
@@ -1,49 +1,72 @@
-console.log("hello");
+const menuBtn = document.querySelector('#nav-icon-container');
+const nav = document.querySelector('.nav-items');
+const nav2 = document.querySelector('nav');
+const bgCover = document.querySelector('body');
+const typewriter = document.querySelector('.hero h2');
 
-const menuBtn = document.querySelector("#nav-icon-container");
-const nav = document.querySelector("nav");
-
-menuBtn.addEventListener("click", () => {
-  menuBtn.classList.toggle("open");
-  nav.classList.toggle("open");
-  console.log(nav);
-  console.log("click");
+menuBtn.addEventListener('click', () => {
+	menuBtn.classList.toggle('open');
+	nav.classList.toggle('open');
+	nav2.classList.toggle('open');
+	console.log(nav);
+	console.log('click');
 });
 
-// // set up text to print, each item in array is new line
-// var aText = new Array(
-// "There are only 10 types of people in the world:",
-// "Those who understand binary, and those who don't"
-// );
-// var iSpeed = 100; // time delay of print out
-// var iIndex = 0; // start printing array at this posision
-// var iArrLength = aText[0].length; // the length of the text array
-// var iScrollAt = 20; // start scrolling up at this many lines
+//Background fade
 
-// var iTextPos = 0; // initialise text position
-// var sContents = ''; // initialise contents variable
-// var iRow; // initialise current row
+let last_known_scroll_position = 0;
+let ticking = false;
 
-// function typewriter()
-// {
-//  sContents =  ' ';
-//  iRow = Math.max(0, iIndex-iScrollAt);
-//  var destination = document.getElementById("typedtext");
+function fade(scroll_pos) {
+	if (scroll_pos > 125) {
+		bgCover.classList.add('faded');
+	} else {
+		bgCover.classList.remove('faded');
+	}
+	console.log(scroll_pos);
+}
 
-//  while ( iRow < iIndex ) {
-//   sContents += aText[iRow++] + '<br />';
-//  }
-//  destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
-//  if ( iTextPos++ == iArrLength ) {
-//   iTextPos = 0;
-//   iIndex++;
-//   if ( iIndex != aText.length ) {
-//    iArrLength = aText[iIndex].length;
-//    setTimeout("typewriter()", 500);
-//   }
-//  } else {
-//   setTimeout("typewriter()", iSpeed);
-//  }
-// }
+window.addEventListener('scroll', function(e) {
+	last_known_scroll_position = window.scrollY;
 
-//ypewriter();
+	if (!ticking) {
+		window.requestAnimationFrame(function() {
+			fade(last_known_scroll_position);
+			ticking = false;
+		});
+
+		ticking = true;
+	}
+});
+
+//Typewriter
+const desc = 'full stack developer and designer'.split('');
+typewriter.textContent = '_';
+let string = '';
+for (let i = 0; i < desc.length; i++) {
+	string += desc[i];
+	if (i === desc.length - 1) type(string);
+	else type(string + '_');
+}
+
+function type(str) {
+	setTimeout(() => {
+		typewriter.textContent = str;
+	}, 75 * str.length);
+}
+
+//hide nav button
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+	let currentScrollPos = window.pageYOffset;
+	if (prevScrollpos > currentScrollPos) {
+		menuBtn.style.opacity = '1';
+		menuBtn.style.pointerEvents = 'auto';
+	} else {
+		if (!menuBtn.classList.contains('open')) {
+			menuBtn.style.opacity = '0';
+			menuBtn.style.pointerEvents = 'none';
+		}
+	}
+	prevScrollpos = currentScrollPos;
+};
